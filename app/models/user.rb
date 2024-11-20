4d6bc18ceba77fb971c367f75e4a7eb087080990
Surve <surve8729@gmail.com>
2024-11-20 17:45:45 +0530

    Add username & password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, :password, presence: true
+  validates :username, length: { in: 4..10 }
+  validates :password, length: { in: 6..10 }
+  validates :username, uniqueness: true
 end
